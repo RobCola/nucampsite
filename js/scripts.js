6056e94bea86b22bc599f8979d128a74c2f53510
Rob Formicola <robertformicola@gmail.com>
2021-12-27 19:54:41 -0800

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,19 @@ $(function () {
 		}
 	});
 });
+
+$(function () {
+	$("#reserveButton").on("click", function () {
+		$("#reserveModal").modal('show');
+	});
+});
+	
+$(function () {
+	$("#loginButton").on("click", function () {
+        $("#loginModal").modal("show");
+    });
+});
+
+
+	
+  
